c80d444e9be19ef8fd38618d0d9f7a43c8506ec2
extf8 <extfield8@gmail.com>
2024-04-29 16:19:11 +0300

    Refactor knightMoves function and update script.js with improved logging and variable names

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 import knightMoves from './knightMoves.js';
 
 // Test cases
-console.log(knightMoves([0, 0], [1, 2]));
 
-console.log(knightMoves([0, 0], [7, 7]));
+const startPosition = [0, 0];
+const endPosition = [7, 7];
+const movesInput = knightMoves(startPosition, endPosition);
+
+const pathOutput = movesInput.path;
+const movesCountOutput = movesInput.movesCount;
+const pathMap = pathOutput
+    .map((square) => `[${square.join(',')}]`)
+    .join(' -> ');
+
+
+let messageStart = `Knight moves will start at ${startPosition}.`;
+let messageEnd = `Knight moves will end at ${endPosition}.`;
+let pathMessage = `Knight's path: ${pathMap}`;
+let countMessage = `Knight took ${movesCountOutput} moves to reach the end.`;
+
+console.log(messageStart);
+console.log(messageEnd);
+console.log(pathMessage);
+console.log(countMessage);
